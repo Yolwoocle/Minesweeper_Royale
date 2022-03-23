ec3608474f846666eb1d9f6ae90df27e9ad9a064
Yolwoocle <leo.bernard2015@hotmail.com>
2022-03-23 10:23:49 +0100

    fix

--- game.lua
+++ game.lua
@@ -4,11 +4,12 @@ local NetworkManager = require "network"
 
 local Game = Class:inherit()
 
---Guigui load font changer si possible pin
-local font_regular = love.graphics.newFont("fonts/Poppins-Regular.ttf", 24)
-love.graphics.setFont(font_regular)
 
 function Game:init()
+	--Guigui load font changer si possible pin
+	local font_regular = love.graphics.newFont("fonts/Poppins-Regular.ttf", 24)
+	love.graphics.setFont(font_regular)
+	
 	self.actors = {}
 	self.actors.board = Board:new()
 
@@ -40,16 +41,6 @@ function Game:draw()
 		actor:draw()
 	end
 
-	-- DEBUGGING
-	love.graphics.setColor(1,1,1)
-	if self.debugmode then
-		love.graphics.print("FPS "..love.timer.getFPS(), 2,2)
-	end
-	if self.network then
-		love.graphics.print(concat('type=',self.network.network_type,",num=",self.network.network.num), 30,30)
-	else
-		love.graphics.print("Select network type (press c/s)", 30,30)
-
 	if self.actors.board.game_over then
 		--RECTANGLE rgb(120,120,233),0.4)
 		love.graphics.setColor(0.3,0.3,0.5,0.7)
@@ -68,6 +59,17 @@ function Game:draw()
 		text_width = font_regular:getWidth(lose_text)
 		text_height = font_regular:getHeight(lose_text)
 		love.graphics.print(lose_text,(WINDOW_WIDTH-text_width)/2,(WINDOW_HEIGHT-text_height)/2)
+	end
+
+	-- DEBUGGING
+	love.graphics.setColor(1,1,1)
+	if self.debugmode then
+		love.graphics.print("FPS "..love.timer.getFPS(), 2,2)
+	end
+	if self.network then
+		love.graphics.print(concat('type=',self.network.network_type,",num=",self.network.network.num), 30,30)
+	else
+		love.graphics.print("Select network type (press c/s)", 30,30)
 	end
 end
 
